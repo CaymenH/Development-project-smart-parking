f36bca86e81f24fad0c71b87109b04c16e870cca
Caymen <caymenhayes1@icloud.com>
2026-03-26 16:48:49 +0000

    security of azure iot central

---
+++ constant.py
@@ -0,0 +1,5 @@
+# connections to iot central
+scope_ID = "0ne0104D930"
+device_ID = "1cude63m1t7"
+Primary_Key = "1sARZTd09Y4Wmns/f1uoqPmMKhP6kaB072AIAC20m6s="
+PROVISIONING_HOST = "global.azure-devices-provisioning.net"
