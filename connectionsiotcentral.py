b0609dd58853ad695ade0c21842bb3ab80d3e18d
CaymenH <caymenhayes1@icloud.com>
2026-04-07 18:32:12 +0100

    updated connectionsiotcentral.py for 2nd device in azure iot central

--- connectionsiotcentral.py
+++ connectionsiotcentral.py
@@ -1,5 +1,5 @@
 # connections to iot central
 scope_ID = "0ne0104D930"
-device_ID = "1cude63m1t7"
-Primary_Key = "1sARZTd09Y4Wmns/f1uoqPmMKhP6kaB072AIAC20m6s="
+device_ID = "1dur1npdids"
+Primary_Key = "tTML5LxX+or1zt0yFF8vNkJiNDm0b2LtFzxZkGEo/D4="
 PROVISIONING_HOST = "global.azure-devices-provisioning.net"
