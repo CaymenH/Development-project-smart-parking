51f4b94a25c587cef6f766e5a8c511a6bca39464
Caymen <caymenhayes1@icloud.com>
2026-04-21 17:19:56 +0100

    Set up streamlit to connect to firebase

--- dashboard_firebase.py
+++ dashboard_firebase.py
@@ -1,2 +1,11 @@
-from firebase_admin import credentials, firestore, storage, db
+import firebase_admin
+from firebase_admin import credentials, firestore, storage
 import streamlit as st
+
+cred = credentials.Certificate(  "/home/c2025778/Rpi_codes/smart-parking-edd43-firebase-adminsdk-fbsvc-236aa62756.json")
+default_app = firebase_admin.initialize_app(cred, {
+    'storageBucket': "smart-parking-edd43.firebasestorage.app"
+})
+
+database = firestore.client(app=default_app)
+bucket = storage.bucket(app=default_app)
