a0a42bbfc4282e04cc7caa2f77a932f4f9b3dae4
Caymen <caymenhayes1@icloud.com>
2026-04-22 18:34:15 +0100

    removed main app code to different file

--- dashboard_firebase.py
+++ dashboard_firebase.py
@@ -12,12 +12,3 @@ bucket = storage.bucket(app=default_app)
 
 
 
-st.write("smart parking application")
-
-main_page = st.Page("main_page.py", title="Select your bay")
-bay_1 = st.Page("bay_1.py", title="parking bay 1 info")
-bay_2 = st.Page("bay_2.py", title="parking bay 2 info")
-bay_3 = st.Page("bay_3.py", title="parking bay 3 info")
-
-pg = st.navigation([main_page, bay_1, bay_2, bay_3])
-pg.run()
