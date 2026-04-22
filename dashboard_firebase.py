753340a8551f17163938846fb11222cd4f4d87d5
Caymen <caymenhayes1@icloud.com>
2026-04-22 22:37:20 +0100

    dashboard started

--- dashboard_firebase.py
+++ dashboard_firebase.py
@@ -1,24 +1,25 @@
 import firebase_admin
 from firebase_admin import credentials, firestore, storage
 import streamlit as st
-
+ 
 cred = credentials.Certificate(  "/home/c2025778/Rpi_codes/smart-parking-edd43-firebase-adminsdk-fbsvc-236aa62756.json")
 default_app = firebase_admin.initialize_app(cred, {
     'storageBucket': "smart-parking-edd43.firebasestorage.app"
 })
-
 database = firestore.client(app=default_app)
 bucket = storage.bucket(app=default_app)
 
 
-docs = database.collection("parking_bay2").stream()
+ 
 
-for doc in docs:
+st.title("Smart Parking Application Dashboard")
+
+bay = st.selectbox("Select Parking Bay", ("Bay 1", "Bay 2", "Bay 3"))
+
+bay_docs = database.collection(f"parking_{bay.lower().replace(' ', '_')}").stream() 
+
+
+for doc in bay_docs:
      st.write(f"{doc.id} => {doc.to_dict()}")
 
-import streamlit as st
 
-docs = database.collection("parking_bay2").stream()
-
-for doc in docs:
-     st.write(f"{doc.id} => {doc.to_dict()}")
