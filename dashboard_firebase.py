12d40ce22803efdf70f0d8f6ffbd99ca7246d03f
Caymen <caymenhayes1@icloud.com>
2026-04-22 16:44:22 +0100

    added pages for each bay

--- dashboard_firebase.py
+++ dashboard_firebase.py
@@ -19,3 +19,13 @@ docs = database.collection("parking_bay2").stream()
 
 for doc in docs:
     print(f"{doc.id} => {doc.to_dict()}")
+
+st.write("smart parking application")
+
+main_page = st.Page("main_page.py", title="Select your bay")
+bay_1 = st.Page("bay_1.py", title="parking bay 1 info")
+bay_2 = st.Page("bay_2.py", title="parking bay 2 info")
+bay_3 = st.Page("bay_3.py", title="parking bay 3 info")
+
+pg = st.navigation([main_page, bay_1, bay_2, bay_3])
+pg.run()
