eed2b68fe1a4636cec7b73bcc3d6e847d5b643ae
Caymen <caymenhayes1@icloud.com>
2026-04-23 15:10:57 +0100

    fixed error with duplicate running of code

--- dashboard_firebase.py
+++ dashboard_firebase.py
@@ -1,11 +1,16 @@
 import firebase_admin
 from firebase_admin import credentials, firestore, storage
 import streamlit as st
- 
-cred = credentials.Certificate(  "/home/c2025778/Rpi_codes/smart-parking-edd43-firebase-adminsdk-fbsvc-236aa62756.json")
-default_app = firebase_admin.initialize_app(cred, {
-    'storageBucket': "smart-parking-edd43.firebasestorage.app"
-})
+
+
+if not firebase_admin._apps:
+    cred = credentials.Certificate("/home/c2025778/Rpi_codes/smart-parking-edd43-firebase-adminsdk-fbsvc-236aa62756.json")
+    default_app = firebase_admin.initialize_app(cred, {
+        'storageBucket': "smart-parking-edd43.firebasestorage.app"
+    })
+else:
+    default_app = firebase_admin.get_app()
+
 database = firestore.client(app=default_app)
 bucket = storage.bucket(app=default_app)
 
